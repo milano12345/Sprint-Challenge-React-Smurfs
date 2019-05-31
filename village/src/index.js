c919b2b267d35f9c5fcd9b27d6164d2e58cdd6bc
milano12345 <dmilano857@gmail.com>
2019-05-31 10:53:20 -0600

    updated post, added in BrowserRouter as Router

--- village/src/index.js
+++ village/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <Router>
     <App />
-    </BrowserRouter>, document.getElementById('root'));
+    </Router>, document.getElementById('root'));
